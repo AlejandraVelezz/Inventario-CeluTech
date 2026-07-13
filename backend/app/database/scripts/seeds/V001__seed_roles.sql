fdb59bd94fe69327c556722c222a7004d86a68d0
Alejandra Velez <montoyavelez0102@gmail.com>
2026-07-13 18:20:10 -0500

    feat(database): crear tabla roles y datos iniciales

---
+++ backend/app/database/scripts/seeds/V001__seed_roles.sql
@@ -0,0 +1,14 @@
+USE inventario_celutech;
+
+INSERT INTO roles
+(nombre, descripcion)
+
+VALUES
+
+('Administrador','Control total del sistema'),
+
+('Gerente','Acceso a reportes e indicadores'),
+
+('Vendedor','Gestión de ventas'),
+
+('Bodeguero','Control del inventario');
